177c9a81438433fdf909e9400d7189490d1904ea
amanvaidya <shanu.divya123@gmail.com>
2025-04-20 23:53:44 +0530

    day 5

---
+++ mixQuestions.js
@@ -0,0 +1,10 @@
+/*
+Given an array with int values and character a and a target, your task is:
+1. replace a with 0
+2. move 0 to end of array
+3. return indexes if values whose sum matches the array.
+Sample input: ['a',2,7,'a',9,11]
+*/
+var mixQuestions = function(nums, target) {
+
+}
